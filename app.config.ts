69e70a075372a8e405babaa8c20bf01f4bf62c8f
Juan Fonseca <juanfonseca993@gmail.com>
2025-06-20 16:08:38 -0600

    enable eas

--- app.config.ts
+++ app.config.ts
@@ -5,16 +5,27 @@ export default {
     name: 'TCG Trader',
     slug: 'tcg-trader',
     version: '1.0.0',
+    owner: 'onlycarry',
     android: {
       package: 'com.onlycarry.tcgtrader',
+      runtimeVersion: '1.0.0',
     },
     ios: {
-      bundleIdentifier: 'com.onlycarry.tcgtrader', // asegúrate de usar el mismo ID para iOS
+      bundleIdentifier: 'com.onlycarry.tcgtrader',
+      runtimeVersion: {
+        policy: 'appVersion',
+      },
     },
-    plugins: ['react-native-iap'], // <-- 👈 ESTA LÍNEA ES LA QUE FALTA
+    updates: {
+      url: 'https://u.expo.dev/0c1f931a-be48-463a-9dae-149b06176d5c',
+    },
+    plugins: ['react-native-iap'],
     extra: {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      eas: {
+        projectId: '0c1f931a-be48-463a-9dae-149b06176d5c',
+      },
     },
   },
 }
